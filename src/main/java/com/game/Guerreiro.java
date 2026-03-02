37a7fbaa1f14bd6363cca5e86dbe7de892ea87a5
Fabiano Gregorio <fgregorio1723@gmail.com>
2026-03-02 16:37:18 -0300

    Game01

--- src/main/java/com/game/Guerreiro.java
+++ src/main/java/com/game/Guerreiro.java
@@ -3,13 +3,35 @@ package com.game;
 public class Guerreiro {
                         //-> ATRIBUTOS BÁSICOS
     Private String nome;
-    Private int Nivel;
-    Private int Experiência;
+    Private int nivel;
+    Private int Experiencia;
 
                         //-> ATRIBUTOS DE COMBATE
     Private int hpMaximo;
     Private int hpAtual;
     Private int forca;
-    Private int Defesa;
+    Private int defesa;
     Private int agilidade;
+    
+    public Guerreiro(String nome){ //-> Construtor
+        this.nome = nome;
+        this.nivel = 1;
+        this.Experiencia = 0;
+
+                        //-> Valores iniciais 
+        this.hpMaximo = 120;
+        this.hpAtual = 120;
+        this.forca = 15;
+        this.defesa = 10;
+        this.agilidade = 8;
+
+    }
+
+    public void atacar(Guerreiro alvo){//-> Metodo de combate
+        Random random = new random();
+
+        int danoBase = this.forca;
+
+        
+    } 
 }
