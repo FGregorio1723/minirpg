5c346568c9603603dd7ab8a3cdb48bb97e181c4f
Fabiano Gregorio <fgregorio1723@gmail.com>
2026-03-02 16:07:37 -0300

    Initial commit

---
+++ src/main/java/com/game/Guerreiro.java
@@ -0,0 +1,15 @@
+package com.game;
+
+public class Guerreiro {
+                        //-> ATRIBUTOS BÁSICOS
+    Private String nome;
+    Private int Nivel;
+    Private int Experiência;
+
+                        //-> ATRIBUTOS DE COMBATE
+    Private int hpMaximo;
+    Private int hpAtual;
+    Private int forca;
+    Private int Defesa;
+    Private int agilidade;
+}
