063e43ec70edf4ac12678231b1132bb5ffa69605
Fabiano Gregorio <fgregorio1723@gmail.com>
2026-03-04 14:48:40 -0300

    Iniciado combate

--- src/main/java/com/game/Combate.java
+++ src/main/java/com/game/Combate.java
@@ -1,10 +1,39 @@
 package com.game;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Combate {
-    
+    private Personagem jogador;
+    private Personagem inimigo;
+
+    private Scanner scanner;
+    private Random random;
+    private int turno = 1;
+
+    public Combate(Personagem jogador, Personagem inimigo) {
+        this.jogador = jogador;
+        this.inimigo = inimigo;
+        this.scanner = new Scanner(System.in);
+        this.random = new Random();
+        this.turno = 1;
 
     }
-    
+    public boolean iniciar(){
+        while (jogador.estaVivo() && inimigo.estaVivo()) { 
+            System.out.println(turno);
+            if ((this.jogador.agilidade) > (this.inimigo.agilidade)) {
+                jogador.atacar(inimigo);
+                inimigo.atacar(jogador);
+            }else {
+                inimigo.atacar(jogador);
+                jogador.atacar(inimigo);
+            }
+        
+         turno ++;   
+        }
+    }
+
+
 }
 
 
@@ -14,14 +43,16 @@ public class Combate {
 
 
 
+//jogador e inimigo são os protagonistas da luta
+//Eles têm métodos (atacar(), receberDano()) que fazem tudo acontecer
+//A classe Combate só orquestra quem age quando
+//scanner permite interação com o jogador
+//random adiciona imprevisibilidade
+//turno dá contexto visual
 
-
-
-
-
-//1️⃣ Controlar a ordem dos turnos
-//2️⃣ Verificar quem ataca primeiro
-//3️⃣ Aplicar dano e verificar mortes
-//4️⃣ Determinar vencedor
-//5️⃣ Dar recompensa (XP) ao vencedor
-//6️⃣ Tratar fugas (opcional)
+// Controlar a ordem dos turnos
+// Verificar quem ataca primeiro
+// Aplicar dano e verificar mortes
+// Determinar vencedor
+// Dar recompensa (XP) ao vencedor
+// Tratar fugas (opcional)
