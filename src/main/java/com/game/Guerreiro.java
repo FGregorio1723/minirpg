93c0b6bfb0cf230763196a687195ce3a8f173cc9
Fabiano Gregorio <fgregorio1723@gmail.com>
2026-03-10 07:27:30 -0300

    .

--- src/main/java/com/game/Guerreiro.java
+++ src/main/java/com/game/Guerreiro.java
@@ -5,7 +5,7 @@ public class Guerreiro extends Personagem {
     public Guerreiro (String nome){
         super(nome);
     }
-
+    @Override
     protected void inicializarAtributos(){
         this.hpMaximo = 140;
         this.forca = 18;
